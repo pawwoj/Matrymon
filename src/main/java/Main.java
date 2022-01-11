85f50fca97584dbd2b45d4f82505180d0732f7e9
pawwoj <pwojdacki84@gmail.com>
2022-01-11 22:33:20 +0100

    Project assumptions added in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,3 +7,29 @@ public class Main {
         app.run();
     }
 }
+/*
+         * Stworz krotki system do zarzadzania programistami i ich partnerkami
+         *
+         * Programista ma imie, nazwisko, pensje oraz lsite jezykow jakie zna
+         *
+         * Kobieta ma imie, nazwsiko, rozmiar biustu oraz IQ
+         *
+         * Napisz metode ktora znajduje top n programistow (jesli chodzi o zarobki)
+         * ktorzy kodza w javie. Jesli nie jestes w stanie tego zrobic rzuc wyjatkiem
+         * NoJavaPorgrammerHere
+         *
+         * Programista moze chodzic na Randke(nazwa, miejsce, kobieta). Napisz metode
+         * ktora wylicza % szanse na zaliczenie na randce. Algorytm zalezy odm iejsca np
+         * restuaracja +10%, kino +20% itd oraz od kobiety, rozmiar A + 10%, B+ 20% itd
+         * oraz od IQ wzor IQ/100. Jesli szansa na zaliczenie wynosi over 100%
+         * programista jest zapewny siebie i jego szanse spdaja, zatem obslugujemy
+         * wyajtek i ustawiamy szanse na zaliczenie do 30%
+         *
+         * Dodaj kobiecie atrybut ulubione miejsce, ktore jest losowane z listy miejsc.
+         Jesli wylosowane ulubione miejsce kobiety jest inne niz to w ktorym zabral je
+         programista powstaje konflikt, BadPlaceForDateException z zastrzezeniem ze
+         moze przyjac on wiadomosc dla programisty od kobiety albo zostac pusty
+         *
+         * + Mozesz stworzyc hierarchie Kobieta -> Partnerka, Zona, Kochanka
+         * + JezykiProgramowania niech beda enumem, Miejsce na randke rowniez enum
+         */
